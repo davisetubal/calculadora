79bb09d3a956185d4057dfe90cad04123d80e085
davi setubal <>
2022-04-29 01:13:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function calcularArea (){
+var input = document.getElementById("raio")
+var raio = input.value
+  
+var raio = 5
+var area = 3.14 * raio **2
+
+var p = document.getElementById("resultado")
+p.innerHTML = area
+}
